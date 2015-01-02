d796908294097c9276d49c95bbd78ffdfce2dafd
Suchit  Puri <suchit.puri@wingify.com>
2015-01-02 14:17:47 +0530

    moving the products route inside the products folder

--- chapter-5/example1/app/controllers/products.js
+++ chapter-5/example1/app/controllers/products.js
@@ -3,5 +3,5 @@ import Ember from "ember";
 export default Ember.ArrayController.extend({
     sortProperties: ['name'],
     sortAscending: true,
-    itemController: 'product'
+    itemController: 'products/product'
 });
