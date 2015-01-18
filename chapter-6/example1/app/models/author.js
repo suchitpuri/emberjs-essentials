d9434ac0bf8f143d5572b97cdefef4661c13d7d5
Suchit  Puri <suchit.puri@wingify.com>
2015-01-18 15:38:28 +0530

    inital commit for example1 chapter6

---
+++ chapter-6/example1/app/models/author.js
@@ -0,0 +1,11 @@
+import DS from "ember-data";
+
+export default DS.Model.extend({
+  firstName: DS.attr('string'),
+  lastName: DS.attr('string'),
+  fullName: function() {
+    return this.get('firstName') + ' ' + this.get('lastName');
+  }.property('firstName', 'lastName'),
+  bio: DS.attr('string'),
+  book: DS.belongsTo('book')
+});
