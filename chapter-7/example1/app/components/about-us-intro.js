8d4c8c2773d4fffa741b5c8ee802b8c54f42966e
Suchit  Puri <suchit.puri@wingify.com>
2015-02-17 15:55:23 +0530

    adding the about us intro component

--- chapter-7/example1/app/components/about-us-intro.js
+++ chapter-7/example1/app/components/about-us-intro.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
     tagName: "p",
+    classNames: ["static-class","another-static-class"],
     classNameBindings: ["intro","text","trueClass","falseClass"],
     intro: "intro-css-class",
     text: "text-css-class",
