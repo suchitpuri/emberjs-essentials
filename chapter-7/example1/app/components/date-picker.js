04f392a9f7632c6ef9273b276b0f2bc87c1185ec
Suchit  Puri <suchit.puri@wingify.com>
2015-02-19 17:11:02 +0530

    changing click to submit

--- chapter-7/example1/app/components/date-picker.js
+++ chapter-7/example1/app/components/date-picker.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
  actions:{
-   click: function(){
+   submit: function(){
     var date = this.$("input").val();
     this.sendAction('action',date);
    } 
