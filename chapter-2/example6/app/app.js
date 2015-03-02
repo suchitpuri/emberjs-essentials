e08d1238803086c568f498e78ffb1345652db6ff
Suchit Puri <suchit.puri@wingify.com>
2015-03-02 23:09:26 +0530

    adding sixth example showing the computed properties behavior

---
+++ chapter-2/example6/app/app.js
@@ -0,0 +1,28 @@
+import Ember from 'ember';
+import Resolver from 'ember/resolver';
+import loadInitializers from 'ember/load-initializers';
+import config from './config/environment';
+import user from './user';
+
+Ember.MODEL_FACTORY_INJECTIONS = true;
+
+var App = Ember.Application.extend({
+  modulePrefix: config.modulePrefix,
+  podModulePrefix: config.podModulePrefix,
+  Resolver: Resolver
+});
+
+loadInitializers(App, config.modulePrefix);
+
+var myUser = user.create({
+  firstName: "suchit",
+  lastName: "puri"
+});
+
+console.log(myUser.get('fullName'));
+myUser.set("firstName","Tony");
+myUser.set("lastName","Stark");
+console.log(myUser.get('fullName'));
+console.log(myUser.get('fullName'));
+
+export default App;
