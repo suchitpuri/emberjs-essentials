56437a70b81e1a481dd3768812f30c61b2941c88
Suchit Puri <suchit.puri@wingify.com>
2015-04-28 18:01:10 +0530

    fixing typo

--- chapter-3/example5/app/routes/index.js
+++ chapter-3/example5/app/routes/index.js
@@ -4,16 +4,16 @@ export default Ember.Route.extend({
   model: function() {
     var companies = [{
       "name" : "Google",
-      "headquaters": "Mountain View, California, United States of America",
+      "headquarters": "Mountain View, California, United States of America",
       "revenue":"59825000000"
       },{
         "name" : "Facebook",
-        "headquaters":"Menlo Park, California,United States of America",
+        "headquarters":"Menlo Park, California,United States of America",
         "revenue":"7870000000"
       },{
         "name" : "twitter",
         "revenue": "664000000",
-        "headquaters":"San Francisco, California, United States of America"
+        "headquarters":"San Francisco, California, United States of America"
       }];
     return companies;
   }
