e9d0ca9c8cc85411fed26496e874d8ab8ad0054c
Suchit Puri <suchitpuri@gmail.com>
2015-07-03 14:36:51 +0530

    Fixing the number issue

--- chapter-2/example8/app/time_manager.js
+++ chapter-2/example8/app/time_manager.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Object.extend({
-  time:00,
+  time:0,
   timeChanged: function(){
     //Do something when time changes
     console.log('time changed');
